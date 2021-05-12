8def6748504bb609fc3d367336be0596a99835bc
Grecia Barandiaran <greciabarandiaran@gmail.com>
2021-05-11 23:11:52 -0500

    segunda version de prueba

--- settings.py
+++ settings.py
@@ -3,23 +3,12 @@ from os import environ
 
 SESSION_CONFIGS = [
     dict(
-        name='public_goods',
-        display_name="Public Goods",
+        name='bertrand',
+        display_name="Bertrand",
         num_demo_participants=3,
-        app_sequence=['public_goods', 'payment_info'],
-    ),
-    dict(
-        name='guess_two_thirds',
-        display_name="Guess 2/3 of the Average",
-        num_demo_participants=3,
-        app_sequence=['guess_two_thirds', 'payment_info'],
-    ),
-    dict(
-        name='survey',
-        display_name='survey',
-        num_demo_participants=1,
-        app_sequence=['survey', 'payment_info'],
-    ),
+        app_sequence=['bertrand', 'payment_info'],
+    )
+    
 ]
 
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
