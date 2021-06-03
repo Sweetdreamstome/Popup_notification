cc7bcc151b18d4f90a4d30bd8213eb97b75705dc
Grecia Barandiaran <greciabarandiaran@gmail.com>
2021-06-03 12:43:25 -0500

    agregar la funcion de 'agregar html desde python'

--- bertrand/templates/lala.py
+++ bertrand/templates/lala.py
@@ -1,30 +1,30 @@
-from bs4 import BeautifulSoup
-lol=open("C:/Users/fredi/Desktop/python/e2labup/bertrandd/bertrand/templates/bertrand/Decide.html","r")
-filee=lol.read()
-
-def agregar_html(page):
-    f1le=open(page,"r+")
-    text=f1le.read()
-    tet=""" 
-    
+def agregar_popup(page):
+    abrir_file=open(page,"r+")
+    script="""
     <script>
         setTimeout(
     function () {
-        alert("Hemos notado el tiempo que te ha tomado en la sección actual del experimento y queremos saber si tienes algún inconveniente, duda o consulta. No olvide que su compañer@ esta esperando a que termine esta sección para avanzar. ");
+        alert("Hemos notado el tiempo que te ha tomado en la seccion actual del experimento y queremos saber si tienes algun inconveniente, duda o consulta. No olvide que su compañer@ esta esperando a que termine esta seccion para avanzar. ");
     },
     6*1000) // 6 seconds
     </script>
+
     """
-    find=text.find(" ")
+    #holi=open("C:/Users/fredi/Desktop/python/e2labup/bertrandd/bertrand/templates/bertrand/Decide.html","r+")
+    texto_file=abrir_file.readlines()
+    pos=texto_file.index('\n')
+    if texto_file[pos+1]== '    <script>\n':
+        pass
+    else:
+        texto_file[pos]=script
+        
+        new_text="".join(texto_file)
+        abrir_file.close()
+        #holii=open("C:/Users/fredi/Desktop/python/e2labup/bertrandd/bertrand/templates/bertrand/Decide.html","w")
+        abrir_2file=open(page,"w")
+        abrir_2file.write(new_text)
+        abrir_2file.close()
     
-
-
     
-    #f1le=open(page,"r+")
-    #for line in f1le:
-    #    if line==" ":
-    #        f1le.write(tet)
-    
-    
-
-agregar=agregar_html("C:/Users/fredi/Desktop/python/e2labup/bertrandd/bertrand/templates/bertrand/Decide.html")
+agregar_popup("C:/Users/fredi/Desktop/python/e2labup/bertrandd/bertrand/templates/bertrand/Decide.html")
+agregar_popup("C:/Users/fredi/Desktop/python/e2labup/bertrandd/bertrand/templates/bertrand/MyWaitPage.html")
