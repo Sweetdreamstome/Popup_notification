3513a6b2faed5f29fb9964d63ac82740152205c4
Grecia Barandiaran <greciabarandiaran@gmail.com>
2021-06-04 10:55:31 -0500

    agregar parametros adicinales a la funcion agregar_popup

--- bertrand/templates/lala.py
+++ bertrand/templates/lala.py
@@ -1,19 +1,11 @@
-def agregar_popup(page):
-    abrir_file=open(page,"r+")
-    script="""
-    <script>
-        setTimeout(
-    function () {
-        alert("Hemos notado el tiempo que te ha tomado en la seccion actual del experimento y queremos saber si tienes algun inconveniente, duda o consulta. No olvide que su compañer@ esta esperando a que termine esta seccion para avanzar. ");
-    },
-    6*1000) // 6 seconds
-    </script>
 
-    """
+def agregar_popup(url, page, seconds):
+    abrir_file=open(url,"r+")
+    script="\n <script>\n setTimeout( \n function(){\n alert('"+str(page)+"');\n },\n "+str(seconds)+"*1000) // "+str(seconds)+" seconds\n </script>\n \n"
     #holi=open("C:/Users/fredi/Desktop/python/e2labup/bertrandd/bertrand/templates/bertrand/Decide.html","r+")
     texto_file=abrir_file.readlines()
     pos=texto_file.index('\n')
-    if texto_file[pos+1]== '    <script>\n':
+    if texto_file[pos+1]== ' <script>\n':
         pass
     else:
         texto_file[pos]=script
@@ -21,10 +13,14 @@ def agregar_popup(page):
         new_text="".join(texto_file)
         abrir_file.close()
         #holii=open("C:/Users/fredi/Desktop/python/e2labup/bertrandd/bertrand/templates/bertrand/Decide.html","w")
-        abrir_2file=open(page,"w")
+        abrir_2file=open(url,"w")
         abrir_2file.write(new_text)
         abrir_2file.close()
     
-    
-agregar_popup("C:/Users/fredi/Desktop/python/e2labup/bertrandd/bertrand/templates/bertrand/Decide.html")
-agregar_popup("C:/Users/fredi/Desktop/python/e2labup/bertrandd/bertrand/templates/bertrand/MyWaitPage.html")
+lista=["Somos el equipo E2LabUP. Hemos notado el tiempo que te ha tomado en la seccion actual del experimento y queremos saber si tienes algun inconveniente, duda o consulta.","Somos el equipo E2LabUP. No te preocupes por el tiempo de espera. Es parte del experimento. En unos momentos, podras avanzar a la siguiente seccion."]
+lista1=[6,3]
+agregar_popup("C:/Users/fredi/Desktop/python/e2labup/bertrandd/bertrand/templates/bertrand/Decide.html",lista[0],lista1[0])
+agregar_popup("C:/Users/fredi/Desktop/python/e2labup/bertrandd/bertrand/templates/bertrand/MyWaitPage.html",lista[1],lista1[1])
+
+text=open("C:/Users/fredi/Desktop/python/e2labup/bertrandd/bertrand/templates/bertrand/Decide.html","r+")
+print(text.readlines())
