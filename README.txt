e81404bb55973a05d57e10051cbf2c2415a07c9e
Grecia Barandiaran <greciabarandiaran@gmail.com>
2021-06-25 17:48:59 -0500

    ultimo intento

--- README.txt
+++
@@ -1,4 +0,0 @@
-Respecto al popup:
-La función agregar_popup está en el branch "gress", el .py se llama "avance_popup": https://github.com/Sweetdreamstome/experimentos_kg/blob/gress/avance_popup.py
-El html que incluye el texto de los popup está en el branch "gress", el .html se llama "texto": https://github.com/Sweetdreamstome/experimentos_kg/blob/gress/texto.html
-
--- avance_popup.py
+++ avance_popup.py
@@ -1,18 +1,20 @@
 import os
-"""
-Si quieres modificar tu template, elimina el texto que se agrega en el template con la funcion de agregar_popup.
-Después que modifiques tu template, asegura que exista un espacio en blanco en una línea específicamente,
-para que el texto del pop up pueda ser escrito y no suceda un error. 
-Luego puedes usar la funcion de agregar_popup 
-(solo se agregará el script en aquel template que lo necesite)
-"""
-def agregar_popup(directorio1,directorio2,nombre_texto,instructions_template,waitpage_template,minutos_pagenormal,minutos_waitpage):
+def agregar_texto_html(texto_a_buscar,texto_file,script,template):   
+    """
+    Se agrega el script2 en la lista que tiene como elementos las líneas del template, posteriormente se junta esta lista, creando un texto llamado 'new text'. 
+    Luego, se abre de nuevo el template con el método "w". Finalmente, se transcribe el template con 'new_text'.
+    """
+    pos=texto_file.index(texto_a_buscar)
+    texto_file[pos]=script
+    new_text="".join(texto_file)       
+    abrir_2file=open(template,"w")
+    abrir_2file.write(new_text)
+    abrir_2file.close()
+
+def agregar_popup():
     """
     Lo que hace la función es agregar un texto en cada template de un proyecto en otree que permita
-    el uso de popups en cada página de los experimentos. Sin embargo, para el caso de los template de
-    instrucciones no se agregará el popup, puesto que usualmente el template de las instrucciones está 
-    en todos los templates. Waitpage tiene otro texto de popup, es por esa razón que se debe crear un
-    waitpage costumizable para poder correr la función.
+    el uso de popups en cada página de los experimentos. 
     Input:
     directorio de los templates de la aplicacion (string), 
     el archivo txt que tiene el cuerpo del pop up (string), 
@@ -24,13 +26,23 @@ def agregar_popup(directorio1,directorio2,nombre_texto,instructions_template,wai
     Output:
     None
     """
-    os.chdir(directorio2) #abres el directorio del texto que contiene los popup en python
+
+    directorio1=input("Por favor, escriba el directorio de los template de la aplicación: ")
+    directorio2=input("Por favor, escriba el directorio del .html que contiene el texto del popup: ")
+    nombre_texto=input("Por favor, escriba el nombre del texto .html: ")
+    instructions_template=input("Si tu template de instrucciones no se repite en otras páginas a parte del de introducción, pon 'No'; caso contrario, escriba el nombre de su template de instrucciones, por favor: ")
+    waitpage_template=input("Por favor, escriba el nombre del template de su waitpage: ")
+    minutos_pagenormal=input("Por favor, escriba los minutos que una persona puede mantenerse respondiendo una pregunta o leyendo en cualquier página: ")
+    minutos_waitpage=input("Por favor, erscriba los minutos que una persona puede esperar en un WaitPage: ")
+
+
+    os.chdir(directorio2) 
     abrir_file_texto=open(nombre_texto,"r+") 
     file_texto=abrir_file_texto.readlines()
-    lista_texto_popup=[] #lista que contiene los textos a leer en el popup
-    lista_minutos=[minutos_pagenormal*60,minutos_waitpage*60] #lista que contiene los minutos de espera antes de que aparezca el popup
-   
-    #creas lista que contiene el texto de cada popup, además eliminas los espacios en blanco dentro del archivo texto.html
+    lista_texto_popup=[]
+    #lista_minutos=[minutos_pagenormal*60,minutos_waitpage*60] 
+    lista_minutos=[minutos_pagenormal,minutos_waitpage]
+    #Creas lista que contiene el texto de cada popup, además eliminas los espacios en blanco dentro del archivo texto.html
     for lista in file_texto:
         tag="\n"
         for word in range(len(tag)):
@@ -38,7 +50,8 @@ def agregar_popup(directorio1,directorio2,nombre_texto,instructions_template,wai
         lista_texto_popup.append(lista)
 
 
-    os.chdir(directorio1) #abres el directorio de los templates en python
+    os.chdir(directorio1) 
+
     #Lista con todos los templates:
     lista_templates=[]
     for fichero in os.scandir(directorio1):
@@ -47,56 +60,28 @@ def agregar_popup(directorio1,directorio2,nombre_texto,instructions_template,wai
     #abrir el file y realizar el procedimiento para cada template dentro del directorio:
     for template in lista_templates: 
    
-        if template==waitpage_template: #si el template es el waitpage....
+        if template==waitpage_template: 
             abrir_file=open(template,"r+")
-            """
-            diferencia entre script y script2:
-            script es para aquellos templates que no contengan {% block content %}
-            script2 es para aquellos templates que sí contienen un {% block content %}
-            """
             script="\n{% block content %}\n \n <script>\n setTimeout( \n function(){\n alert('"+str(lista_texto_popup[1])+"');\n },\n "+str(lista_minutos[1])+"*1000) // "+str(lista_minutos[1])+" segundos\n </script>\n \n{% endblock %}\n \n"
             script2="\n{% block content %}\n \n <script>\n setTimeout( \n function(){\n alert('"+str(lista_texto_popup[1])+"');\n },\n "+str(lista_minutos[1])+"*1000) // "+str(lista_minutos[1])+" segundos\n </script>\n \n"
-            texto_file=abrir_file.readlines() #lista con cada línea del html en cada elemento.
+            texto_file=abrir_file.readlines()
 
-            if " alert('"+str(lista_texto_popup[1])+"');\n" in texto_file: #si ya se escribió la función alert previamente, no se hará nada
+            if " alert('"+str(lista_texto_popup[1])+"');\n" in texto_file: 
                 pass
             else:
                 if '{% block content %}\n' in texto_file:
-                    """
-                    Se agrega el script2 en la lista que tiene como elementos las líneas del template, 
-                    posteriormente se junta esta lista, creando un texto llamado 'new text'. 
-                    Luego, se abre de nuevo el template, pero solo con el método "w". 
-                    Finalmente, se transcribe el template con 'new_text', que contiene todo el texto del template incluido 
-                    el script2.
-                    """
-                    pos=texto_file.index('{% block content %}\n')#posición del {%block content%}
-                    texto_file[pos]=script2 #en la línea donde está el {%block content%} se escribirá un nuevo texto que contiene el texto del script2
-                    new_text="".join(texto_file) #juntas la lista en un texto
-                    abrir_file.close() 
-                    abrir_segundo_file=open(template,"w") #abres por segunda vez el archivo, pero solo con el método write
-                    abrir_segundo_file.write(new_text) #transcribes 
-                    abrir_segundo_file.close()
+                    
+                    agregar_texto_html('{% block content %}\n',texto_file,script2,template)
            
                 else:
-                    """
-                    Se agrega el script en la lista que tiene como elementos las líneas del template, 
-                    posteriormente se junta esta lista, creando un texto llamado 'new text'. 
-                    Luego, se abre de nuevo el template, pero solo con el método "w". 
-                    Finalmente, se transcribe el template con 'new_text', que contiene todo el texto del template incluido 
-                    el script.
-                    """
-                    pos=texto_file.index('\n')#posición del primer espacio en blanco del template
-                    texto_file[pos]=script
-                    new_text="".join(texto_file)
-                    abrir_file.close()
-                    abrir_2file=open(template,"w")
-                    abrir_2file.write(new_text)
-                    abrir_2file.close()
-        
-        elif template==instructions_template: #no escribir el 'script' o 'script2' en el template de instrucciones
+                    agregar_texto_html('\n',texto_file,script,template)
+                    
+    
+        elif instructions_template!='No':#no escribir el 'script' o 'script2' en el template de instrucciones
             pass
         
         else:
+
             abrir_file=open(template,"r+")
             script="\n{% block content %}\n \n <script>\n setTimeout( \n function(){\n alert('"+str(lista_texto_popup[0])+"');\n },\n "+str(lista_minutos[0])+"*1000) // "+str(lista_minutos[0])+" segundos\n </script>\n \n{% endblock %}\n \n"
             script2="\n{% block content %}\n \n <script>\n setTimeout( \n function(){\n alert('"+str(lista_texto_popup[0])+"');\n },\n "+str(lista_minutos[0])+"*1000) // "+str(lista_minutos[0])+" segundos\n </script>\n \n"
@@ -104,42 +89,16 @@ def agregar_popup(directorio1,directorio2,nombre_texto,instructions_template,wai
 
             if " alert('"+str(lista_texto_popup[0])+"');\n" in texto_file: #si ya se escribió la función alert previamente, no se hará nada
                 pass
+            
             else:
+                    
                 if '{% block content %}\n' in texto_file:
-                    """
-                    Se agrega el script2 en la lista que tiene como elementos las líneas del template, 
-                    posteriormente se junta esta lista, creando un texto llamado 'new text'. 
-                    Luego, se abre de nuevo el template, pero solo con el método "w". 
-                    Finalmente, se transcribe el template con 'new_text', que contiene todo el texto del template incluido 
-                    el script2.
-                    """
-                    pos=texto_file.index('{% block content %}\n')
-                    #se va a sobreescribir el texto en el html, por eso se usa el método write.
-                    texto_file[pos]=script2
-                    new_text="".join(texto_file)
-                    abrir_file.close()
-                    abrir_2file=open(template,"w")
-                    abrir_2file.write(new_text)
-                    abrir_2file.close()
-           
+                    agregar_texto_html('{% block content %}\n',texto_file,script2,template)
+                    
                 else:
-                    """
-                    Se agrega el script en la lista que tiene como elementos las líneas del template, 
-                    posteriormente se junta esta lista, creando un texto llamado 'new text'. 
-                    Luego, se abre de nuevo el template, pero solo con el método "w". 
-                    Finalmente, se transcribe el template con 'new_text', que contiene todo el texto del template incluido 
-                    el script.
-                    """
-                    pos=texto_file.index('\n')#posición del primer espacio en blanco del template
-                    #se va a sobreescribir el texto en el html, por eso se usa el método write.
-                    texto_file[pos]=script
-                    new_text="".join(texto_file)
-                    abrir_file.close()
-                    abrir_2file=open(template,"w")
-                    abrir_2file.write(new_text)
-                    abrir_2file.close()
+                    agregar_texto_html('\n',texto_file,script,template)
+                
 
-agregar_popup("C:/Users/fredi/Desktop/python/e2labup/bertrandd/popup/templates/popup","C:/Users/fredi/Desktop/python/e2labup/bertrandd","C:/Users/fredi/Desktop/python/e2labup/bertrandd/texto.html","instructions.html","MyWaitPage.html")
+#agregar_popup("C:/Users/fredi/Desktop/python/e2labup/bertrandd/popup/templates/popup","C:/Users/fredi/Desktop/python/e2labup/bertrandd","C:/Users/fredi/Desktop/python/e2labup/bertrandd/texto.html","instructions.html","MyWaitPage.html",6,3)
 
-#l=open("C:/Users/fredi/Desktop/python/e2labup/bertrandd/popup/templates/popup/MyWaitPage.html","r+")
-#print(l.readlines())
+agregar_popup()
